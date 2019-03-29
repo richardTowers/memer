263613cdfbf477039cfbe5b3dc4f149e7ae14e45
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-03-29 16:55:02 +0000

    Multiple text fields for multiple places

--- static/memer.js
+++ static/memer.js
@@ -2,23 +2,43 @@
 
 const canvas= document.getElementById('meme-canvas')
 const context = canvas.getContext('2d')
+context.font = '30px monospace'
+context.fillStyle = 'white'
+context.strokeStyle = 'black'
+context.lineWidth = 2
 let state
 
 function renderImage () {
-    context.drawImage(this, 0, 0, 200, 200)
-    state = context.getImageData(0, 0, 200, 200)
+    context.drawImage(this, 0, 0, 500, 333)
+    state = context.getImageData(0, 0, 500, 333)
 }
 
 window.onload = function () {
     const firstImage = document.querySelector('img')
-    renderImage.call(firstImage)
+    if (firstImage) {
+     renderImage.call(firstImage)   
+    }
 }
 
 for (const img of document.querySelectorAll('img.meme-template')) {
     img.onclick = renderImage
 }
 
-document.querySelector('#meme-text').onkeyup = function () {
-    context.putImageData(state, 0, 0)
-    context.fillText(this.value, 50, 50)
+const textBoxes = Array.from(document.querySelectorAll('.meme-text'))
+
+const positions = [
+    [80, 250],
+    [250, 150],
+    [400, 200],
+]
+
+for (let i = 0; i < textBoxes.length; i++) {
+    textBoxes[i].onkeyup = function () {
+        context.putImageData(state, 0, 0)
+        context.strokeText(this.value, positions[i][0], positions[i][1])
+        context.fillText(this.value, positions[i][0], positions[i][1])
+    }
+    textBoxes[i].onblur = function () {
+        state = context.getImageData(0, 0, 500, 333)
+    }
 }
