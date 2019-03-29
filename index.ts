8b7d1a9565cb2d65b764a8cc62bf679709c9a46f
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-03-29 10:45:09 +0000

    Addd the aws-sdk so we can interact with S3 buckets

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import { S3 } from 'aws-sdk'
 
 const app = express()
 
-app.get('/', (_req, res) => {
-  res.send('Hello world!')
+app.get('/', async (_req, res, next) => {
+  try {
+    const s3 = new S3({apiVersion: '2006-03-01'})
+    const buckets = await s3.listBuckets().promise()
+    if(buckets.Buckets) {
+      res.send('Buckets: ' + buckets.Buckets.map(x => x.Name).join(', '))
+    } else {
+      res.send('No buckets :(')
+    }
+  } catch(e) {
+    next(e)
+  }
 })
 
 app.listen(3000, () => console.log('Listening on 3000'))
