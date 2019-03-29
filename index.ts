f32a97f3c8d7b20b9d19c8e159e38cd6464827ef
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-03-29 14:26:23 +0000

    Very basic image upload / download functionality

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import multer from 'multer'
+import serveStatic from 'serve-static'
 import { S3 } from 'aws-sdk'
+import fs from 'fs'
 
 const port = process.env['PORT'] || '3000'
 
@@ -12,13 +15,36 @@ const credentials = (vcapServices['aws-s3-bucket'] || [{}])[0]['credentials']
 if (!credentials) {
   throw new Error('VCAP_SERVICES must contain aws-s3-bucket[0].credentials, found ' + Object.keys(vcapServices))
 }
-const { aws_access_key_id: accessKeyId, aws_secret_access_key: secretAccessKey, aws_region: region, bucket_name: bucketName } = credentials
+const { bucket_name: bucketName } = credentials
+const s3 = new S3({
+  accessKeyId: credentials.aws_access_key_id,
+  secretAccessKey: credentials.aws_secret_access_key,
+  region: credentials.aws_region,
+  apiVersion: '2006-03-01'
+})
 
 const app = express()
 
+app.use('/views', serveStatic('views/'))
+
+const imageUpload = multer({ dest: 'uploads/' }).single('imageUpload')
+
+app.post('/upload', imageUpload, async (req, res, next) => {
+  try {
+    const file = fs.readFileSync(__dirname + '/' + req.file.path)
+    await s3.upload({Bucket: bucketName, Key: `${req.file.filename}-${req.file.originalname}`, Body: file}).promise()
+    res.redirect('/')
+  } catch(e) {
+    next(e)
+  }
+})
+
+app.get('/images/:key', (req, res) => {
+  s3.getObject({Bucket: bucketName, Key: req.params.key}).createReadStream().pipe(res)
+})
+
 app.get('/list-objects', async (_req, res, next) => {
   try {
-    const s3 = new S3({accessKeyId, secretAccessKey, region, apiVersion: '2006-03-01'})
     const objects = await s3.listObjects({Bucket: bucketName }).promise()
     if(objects.Contents) {
       res.send('Objects: ' + JSON.stringify(objects.Contents.map(x => x.Key)))
