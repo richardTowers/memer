c7cb80815b31a834185b2a8cf0673274f595e797
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-03-29 16:08:01 +0000

    Very basic text-on-image JS
    Scaling's all horrible and everything, but who cares.

--- static/memer.js
+++ static/memer.js
@@ -1,1 +1,24 @@
-console.log(document.getElementById('meme-canvas'))
+'use strict'
+
+const canvas= document.getElementById('meme-canvas')
+const context = canvas.getContext('2d')
+let state
+
+function renderImage () {
+    context.drawImage(this, 0, 0, 200, 200)
+    state = context.getImageData(0, 0, 200, 200)
+}
+
+window.onload = function () {
+    const firstImage = document.querySelector('img')
+    renderImage.call(firstImage)
+}
+
+for (const img of document.querySelectorAll('img.meme-template')) {
+    img.onclick = renderImage
+}
+
+document.querySelector('#meme-text').onkeyup = function () {
+    context.putImageData(state, 0, 0)
+    context.fillText(this.value, 50, 50)
+}
